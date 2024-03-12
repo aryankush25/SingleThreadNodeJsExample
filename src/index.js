27de03426c105d529661a59356287b15b80d3c4f
Aryan Agarwal <aryankush025@gmail.com>
2024-03-12 13:27:36 +0530

    Add delay to response time

--- src/index.js
+++ src/index.js
@@ -4,9 +4,28 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  const now = new Date();
+  const delay = addSeconds(now, 5);
+  console.log(`Request received at ${now}`);
+  console.log(`Delay is ${delay}`);
+
+  let count = 0;
+
+  while (new Date() < delay) {
+    count++;
+  }
+
+  res.send("Count " + count);
 });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+function addMinutes(date, minutes) {
+  return new Date(date.getTime() + minutes * 60000);
+}
+
+function addSeconds(date, seconds) {
+  return new Date(date.getTime() + seconds * 1000);
+}
